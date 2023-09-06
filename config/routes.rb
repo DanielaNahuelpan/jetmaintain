26820b36a2695a9ee1a839a25d188ce40ddaa759
Daniela <daniela.paz.n.s@gmail.com>
2023-09-05 22:52:50 -0300

    Agrego devise y navbar funcionando correctamente

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'homes/index'
+  get 'homes/terms'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "homes#index"
 end
